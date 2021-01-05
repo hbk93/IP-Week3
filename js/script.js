f95f12adcf089684d593f8cf58c6aca60acb2a6a
Harry Bett <harrybett89@gmail.com>
2021-01-05 14:27:27 +0300

    format document

--- js/script.js
+++ js/script.js
@@ -1,22 +1,88 @@
-// $(document).ready(function(){
-//   $("#design-image").click(function(){
-//     $("#design-image").slideDown('1500').hide('1000');
-//     $("#design").show('1500');
-//   });
-
-// $(document).ready(function() {
-//   $("#design").click(function() {
-//     $("#design").show();
-//   });
-// });
-
-$(document).ready(function(){  
-  $("#design").click(function(){
-    $("#design").toggle();
-    $("#design").toggle();
+$(document).ready(function () {
+  $("#development_image").click(function () {
+    $("#development_image").slideDown('1500').hide('1000');
+    $("#development").show('1500');
   });
-  $("#design_image").click(function(){
+  $("#development").click(function () {
+    $("#development").slideUp('1500');
+    $("#development_image").slideDown('1500');
+  });
+});
+
+$(document).ready(function () {
+  $("#design_image").click(function () {
     $("#design_image").slideDown('1500').hide('1000');
-    $("#design_image").show('1500');
+    $("#design").show('1500');
+  });
+  $("#design").click(function () {
+    $("#design").slideUp('1500');
+    $("#design_image").slideDown('1500');
+  });
+});
+
+$(document).ready(function () {
+  $("#product_image").click(function () {
+    $("#product_image").slideDown('1500').hide('1000');
+    $("#product").show('1500');
+  });
+  $("#product").click(function () {
+    $("#product").slideUp('1500');
+    $("#product_image").slideDown('1500');
+  });
+});
+$(document).ready(function () {
+  $("#work1").mouseover(function () {
+    $("#overlay_1").show();
+  }).mouseout(function () {
+    $("#overlay_1").hide();
+  });
+});
+$(document).ready(function () {
+  $("#work2").mouseover(function () {
+    $("#overlay_2").show();
+  }).mouseout(function () {
+    $("#overlay_2").hide();
+  });
+});
+$(document).ready(function () {
+  $("#work3").mouseover(function () {
+    $("#overlay_3").show();
+  }).mouseout(function () {
+    $("#overlay_3").hide();
+  });
+});
+$(document).ready(function () {
+  $("#work4").mouseover(function () {
+    $("#overlay_4").show();
+  }).mouseout(function () {
+    $("#overlay_4").hide();
+  });
+});
+$(document).ready(function () {
+  $("#work5").mouseover(function () {
+    $("#overlay_5").show();
+  }).mouseout(function () {
+    $("#overlay_5").hide();
+  });
+});
+$(document).ready(function () {
+  $("#work6").mouseover(function () {
+    $("#overlay_6").show();
+  }).mouseout(function () {
+    $("#overlay_6").hide();
+  });
+});
+$(document).ready(function () {
+  $("#work7").mouseover(function () {
+    $("#overlay_7").show();
+  }).mouseout(function () {
+    $("#overlay_7").hide();
+  });
+});
+$(document).ready(function () {
+  $("#work8").mouseover(function () {
+    $("#overlay_8").show();
+  }).mouseout(function () {
+    $("#overlay_8").hide();
   });
 });
